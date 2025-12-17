33d2da2259533ac8a68cc7bcf2c850c63710b0ce
Seven <123136838+Seevvveen@users.noreply.github.com>
2025-12-16 20:21:25 -0500

    Create ScryfallClient

--- code/Scryfall/Base/Scryfall.cs
+++ code/Scryfall/Base/Scryfall.cs
@@ -1,18 +1,66 @@
 ﻿global using Sandbox.Scryfall.Types;
 global using System;
 global using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Sandbox.Scryfall;
 
-public static class Endpoints
-{
-	public const string Base = "https://api.scryfall.com";
-	public const string Sets = Base + "/sets/";
-	public const string Cards = Base + "/cards/";
-	//Symbols
-	//Catalogs
-	public const string BulkData = Base + "/bulk-data/";
-}
 
+// Never Null
+// Data Structures
+// Readable
+/// <summary>
+/// Interact with the scryfall API
+/// </summary>
+public class ScryfallClient
+{
+	private const string BaseUrl = "https://api.scryfall.com";
+	private readonly SemaphoreSlim _gate = new( 1, 1 );
+	private TimeSince _sinceLast;
+	/// <summary> Waits 100ms between api calls to limit api spam</summary>
+	/// <example>await CheckDelay();</example>
+	public async Task ApiDelay()
+	{
+		await _gate.WaitAsync();
+		try
+		{
+			const float interval = 0.1f;
+			if ( _sinceLast < interval )
+				await Task.Delay( (int)((interval - _sinceLast) * 1000f) );
+			_sinceLast = 0;
+		}
+		finally
+		{
+			_gate.Release();
+		}
+	}
+
+	/// <summary> Foundational Api Request </summary>
+	public async Task<T> FetchAsync<T>( string endpoint ) where T : class
+	{
+		await ApiDelay();
+		var response = await Http.RequestJsonAsync<T>( $"{BaseUrl}/{endpoint}" )
+			?? throw new InvalidOperationException( $"[Scryfall] Failed to deserialize response from {BaseUrl}/{endpoint}" );
+		return response;
+	}
+
+	public async Task<T> FetchUrlAsync<T>( string Url ) where T : class
+	{
+		await ApiDelay();
+		var response = await Http.RequestJsonAsync<T>( Url )
+			?? throw new InvalidOperationException( $"[Scryfall] Failed to deserialize {Url}" );
+		return response;
+	}
+
+	/// <summary></summary>
+	public Task<Card> GetCardAsync( string id )
+		=> FetchAsync<Card>( $"cards/{id}" );
+
+	/// <summary></summary>
+	public Task<ApiList<Card>> SearchCardsAsync( string query )
+		=> FetchAsync<ApiList<Card>>( $"cards/search?q={Uri.EscapeDataString( query )}" );
+
+}
 
 
