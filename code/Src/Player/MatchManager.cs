247b5777f1feb6e26acf33258761794b7d0145e9
Seven <123136838+Seevvveen@users.noreply.github.com>
2026-01-11 15:12:30 -0500

    upto daate

---
+++ code/Src/Player/MatchManager.cs
@@ -0,0 +1,44 @@
+﻿namespace Sandbox.Player;
+
+public sealed class MatchManager : Component, Component.INetworkListener
+{
+	public static MatchManager Instance { get; private set; }
+	
+	[Property, Group("Prefabs")] public GameObject PlayerPrefab { get; set; }
+	
+	[Sync( SyncFlags.FromHost )] public int PlayerCount { get; set; }
+
+	public PlayerState[] Players => Scene.GetAllComponents<PlayerState>().ToArray();
+
+	protected override void OnAwake()
+	{
+		Instance = this;
+	}
+
+	public void OnActive( Connection channel )
+	{
+		if ( !Networking.IsHost ) return;
+		if ( PlayerPrefab is null ) return;
+
+		SpawnPlayerFor( channel );
+	}
+
+	private void SpawnPlayerFor( Connection channel )
+	{
+		var seat = Players.Length;
+		
+		var go = PlayerPrefab.Clone( new CloneConfig { Name = channel.DisplayName, } );
+
+		go.Network.SetOrphanedMode( NetworkOrphaned.ClearOwner );
+		go.NetworkSpawn( channel );
+		
+		var ps = go.Components.Get<PlayerState>();
+		ps.SeatIndex = seat;
+		ps.Life = 40;
+		ps.IsReady = false;
+
+		PlayerCount = Players.Length;
+		Log.Info( $"Host spawned player seat={seat} owner={channel.DisplayName} ownerId={channel.Id}" );
+	}
+	
+}
---
+++ code/Src/Player/PlayerState.cs
@@ -0,0 +1,38 @@
+﻿namespace Sandbox.Player;
+
+/// <summary>
+/// The "State" of the player, this is not the instance of them in world just data
+/// </summary>
+public class PlayerState : Component
+{
+	
+	//Sets up a Local variable - each client will resolve local to their instance of it
+	private static PlayerState _local;
+	public static PlayerState Local
+	{
+		get
+		{
+			if ( !_local.IsValid() )
+			{
+				_local = Game.ActiveScene
+				             .GetAllComponents<PlayerState>()
+				             .FirstOrDefault( p => p.Network.IsOwner );
+			}
+			return _local;
+		}
+	}
+	
+	//Establish a list of all OTHER players per each player
+	public static PlayerState[] All =>
+		Game.ActiveScene.GetAllComponents<PlayerState>().Where( p => p.IsValid ).ToArray();
+	
+	//Properties consistent across all PlayerStates
+	[Sync] public int SeatIndex { get; set; }
+	[Sync] public int Life { get; set; } = 40;
+	[Sync] public bool IsReady { get; set; }
+
+	protected override void OnStart()
+	{
+		Log.Info( $"PlayerState OnStart: seat={SeatIndex} ownerId={Network.OwnerId} isOwner={Network.IsOwner} isProxy={IsProxy}" );
+	}
+}
