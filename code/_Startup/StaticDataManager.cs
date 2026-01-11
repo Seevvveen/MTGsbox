a6de32c5167dc8c33ef3525b60577cdca2f8b05e
Seven <123136838+Seevvveen@users.noreply.github.com>
2026-01-11 15:10:59 -0500

    teas

---
+++ code/_Startup/StaticDataManager.cs
@@ -0,0 +1,35 @@
+﻿namespace Sandbox._Startup;
+
+/// <summary>
+/// </summary>
+public sealed class StaticDataManager : GameObjectSystem<StaticDataManager>, ISceneStartup
+{
+	public StaticDataManager( Scene scene ) : base( scene )
+	{
+		
+	}
+
+	
+	
+	
+	
+	
+	// Before Scene Load
+	void ISceneStartup.OnHostPreInitialize(SceneFile scene)
+	{
+		
+	}
+	
+	// Scene Load Computer in Charge of the game - Not Called on connecting clients
+	void ISceneStartup.OnHostInitialize()
+	{
+		
+	}
+	
+	// Scene Load on Host(non-dedicated) and connecting clients
+	void ISceneStartup.OnClientInitialize()
+	{
+		
+	}
+
+}
